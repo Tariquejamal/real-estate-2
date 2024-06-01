4c625f16edf8e98f255771505cefeca1a4110292
Tarique Jamal <92308923+Tariquejamal@users.noreply.github.com>
2024-06-01 06:51:01 +0530

    add sign out fuctionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -74,3 +74,13 @@ export const google = async (req, res, next) => {
       res.status(500).json({ error: 'An error occurred while signing in with Google' });
     }
   };
+
+
+  export const signOut = async (req, res, next) => {
+    try {
+      res.clearCookie('access_token');
+      res.status(200).json('User has been logged out!');
+    } catch (error) {
+      next(error);
+    }
+  };
