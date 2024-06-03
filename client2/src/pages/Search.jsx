3013a29d15bb878635f55be7e9c3dff2154aff12
Tarique Jamal <92308923+Tariquejamal@users.noreply.github.com>
2024-06-03 23:46:31 +0530

    add show more listing functionality

--- client2/src/pages/Search.jsx
+++ client2/src/pages/Search.jsx
@@ -119,6 +119,21 @@ export default function Search() {
         const searchQuery = urlParams.toString();
         navigate(`/search?${searchQuery}`);
       };
+
+
+      const onShowMoreClick = async () => {
+        const numberOfListings = listings.length;
+        const startIndex = numberOfListings;
+        const urlParams = new URLSearchParams(location.search);
+        urlParams.set('startIndex', startIndex);
+        const searchQuery = urlParams.toString();
+        const res = await fetch(`/api/listing/get?${searchQuery}`);
+        const data = await res.json();
+        if (data.length < 9) {
+          setShowMore(false);
+        }
+        setListings([...listings, ...data]);
+      };
     
 
   return (
@@ -229,6 +244,15 @@ export default function Search() {
             listings.map((listing) => (
               <ListingItem key={listing._id} listing={listing} />
             ))}
+
+          {showMore && (
+            <button
+              onClick={onShowMoreClick}
+              className='text-green-700 hover:underline p-7 text-center w-full'
+            >
+              Show more
+            </button>
+          )}
           </div>
         </div>
     </div>
